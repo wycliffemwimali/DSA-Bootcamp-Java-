7a51f6c6b199028172e85dfa42b6bd339f578d73
Wycliffe Mwimali <wycliffemwimali2@gmail.com>
2023-03-31 21:01:23 +0300

    ArrayList

---
+++ src/ArrayListExample.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(59);
+        list.add(34);
+        list.add(45);
+        list.add(890);
+        System.out.println(list);
+    }
+}
