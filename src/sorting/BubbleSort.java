f5cf371318dc38b9d8bec61da5de53271ae48f1d
Wycliffe Mwimali <wwakhungu@safaricom.co.ke>
2023-03-31 20:55:42 +0300

    bubblesort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {-13, 56, 67, 3, 4, 9, 22};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void bubble(int[] arr ){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 1; j < arr.length-i; j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+}
