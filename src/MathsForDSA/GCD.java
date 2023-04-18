20d4c2cb8b6639db313d48487f047bd7a9873181
Wycliffe Mwimali <wycliffemwimali2@gmail.com>
2023-04-18 19:12:28 +0300

    euclid's algorithm

---
+++ src/MathsForDSA/GCD.java
@@ -0,0 +1,13 @@
+package MathsForDSA;
+
+public class GCD {
+    public static void main(String[] args) {
+        System.out.println(gcd(3, 9));
+    }
+    static int gcd(int a, int b){
+        if(a == 0){
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+}
