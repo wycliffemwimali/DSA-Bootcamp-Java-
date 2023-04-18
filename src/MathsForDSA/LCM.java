14541ef2624f6c37d71b32d9daa499c010979ddd
Wycliffe Mwimali <wycliffemwimali2@gmail.com>
2023-04-18 19:34:27 +0300

    lcm of two numbers

---
+++ src/MathsForDSA/LCM.java
@@ -0,0 +1,16 @@
+package MathsForDSA;
+
+public class LCM {
+    public static void main(String[] args) {
+        System.out.println(lcm(9, 18));
+    }
+    static  int gcd(int a, int b){
+        if(a == 0){
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+    static int lcm(int a, int b){
+        return a * b/gcd(a,b);
+    }
+}
