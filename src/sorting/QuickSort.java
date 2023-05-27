87cfd54a2a4d5c0191df47a5c93e3d88de0fc314
Wycliffe Mwimali <wycliffemwimali2@gmail.com>
2023-05-27 14:54:16 +0300

    base condition

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -3,5 +3,11 @@ package sorting;
 public class QuickSort {
     public static void main(String[] args) {
 
+
+    }
+    static  void sort(int[] nums, int low, int hi){
+        if(low>=hi){
+            return;
+        }
     }
 }
