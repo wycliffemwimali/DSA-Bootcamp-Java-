9005787c9a98156a529d9c17f91721f325f98eba
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-02 18:22:29 +0300

    base condition

--- src/Strings/Subseq.java
+++ src/Strings/Subseq.java
@@ -4,4 +4,10 @@ public class Subseq {
     public static void main(String[] args) {
 
     }
+    static void subseq(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+    }
 }
