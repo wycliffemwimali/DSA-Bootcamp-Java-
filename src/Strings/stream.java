7c89a82bc54e29b397d3d73064938d9c50b76f19
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-02 17:17:47 +0300

    stream

---
+++ src/Strings/stream.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class stream {
+    public static void main(String[] args) {
+
+    }
+    static void skip(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+    }
+}
