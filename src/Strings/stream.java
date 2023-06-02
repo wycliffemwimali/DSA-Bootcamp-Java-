23ad49e6d427057ce5b0ee68c0baf439635d10aa
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-02 17:22:54 +0300

    skip method

--- src/Strings/stream.java
+++ src/Strings/stream.java
@@ -9,5 +9,12 @@ public class stream {
             System.out.println(p);
             return;
         }
+        char ch = up.charAt(0);
+
+        if (ch == 'a'){
+            skip(p, up.substring(1));
+        }else {
+            skip(p + ch, up.substring(1));
+        }
     }
 }
