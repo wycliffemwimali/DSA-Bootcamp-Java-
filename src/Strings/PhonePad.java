3f99669d1e6adc7a50d0080f99378d048c921a49
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-04 13:05:12 +0300

    recursion call

--- src/Strings/PhonePad.java
+++ src/Strings/PhonePad.java
@@ -9,5 +9,11 @@ public class PhonePad {
             System.out.println(p);
             return;
         }
+        int digit = up.charAt(0) - '0'; //this will convert '2' into 2
+
+        for(int i = (digit - 1)*3; i < digit * 3; i++){
+            char ch = (char)('a' + i);
+            pad(p + ch, up.substring(1));
+        }
     }
 }
