3acbc7bf5fecef848b703f471580243f17267bef
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-04 13:37:45 +0300

    base condition

--- src/Strings/DiceArrayList.java
+++ src/Strings/DiceArrayList.java
@@ -1,4 +1,17 @@
 package Strings;
 
+import java.util.ArrayList;
+
 public class DiceArrayList {
+    public static void main(String[] args) {
+
+    }
+    static ArrayList<String> diceRet(String p, int target){
+        if(target==0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+    }
 }
