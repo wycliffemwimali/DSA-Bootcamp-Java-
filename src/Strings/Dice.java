498433b8f223cc8de46f6e00315076e52e03c0c4
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-04 13:44:51 +0300

    refactored

--- src/Strings/Dice.java
+++ src/Strings/Dice.java
@@ -4,13 +4,13 @@ public class Dice {
     public static void main(String[] args) {
         dice("", 4);
     }
-    static void dice(String p, int target){
-        if(target==0){
+    static void dice(String p, int face){
+        if(face==0){
             System.out.println(p);
             return;
         }
-        for(int i = 1; i <= 6 && i <= target; i++){
-            dice(p + i, target - i);
+        for(int i = 1; i <= 6 && i <= face; i++){
+            dice(p + i, face - i);
         }
     }
 }
