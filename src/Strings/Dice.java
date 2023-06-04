51f7b3f882b19ea5ca93d34d0159942a9955d4fb
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-04 13:24:30 +0300

    base condition

--- src/Strings/Dice.java
+++ src/Strings/Dice.java
@@ -1,4 +1,13 @@
 package Strings;
 
 public class Dice {
+    public static void main(String[] args) {
+
+    }
+    static void dice(String p, int target){
+        if(target==0){
+            System.out.println(p);
+            return;
+        }
+    }
 }
