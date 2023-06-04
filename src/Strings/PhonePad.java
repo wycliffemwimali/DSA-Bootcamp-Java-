4b0d2390fc82d23ab558348b25bc6e40d163ec60
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-04 12:59:39 +0300

    base condition

--- src/Strings/PhonePad.java
+++ src/Strings/PhonePad.java
@@ -1,4 +1,13 @@
 package Strings;
 
 public class PhonePad {
+    public static void main(String[] args) {
+
+    }
+    static void pad(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+    }
 }
