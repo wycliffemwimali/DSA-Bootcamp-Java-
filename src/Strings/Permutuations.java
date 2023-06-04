9315edc3640bffbda9a06d51e15ffc746928b5d3
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-04 11:52:53 +0300

    base condition

--- src/Strings/Permutuations.java
+++ src/Strings/Permutuations.java
@@ -4,5 +4,14 @@ public class Permutuations {
     public static void main(String[] args) {
 
     }
+    static void permutuations(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
 
+        char ch = up.charAt(0);
+
+
+    }
 }
