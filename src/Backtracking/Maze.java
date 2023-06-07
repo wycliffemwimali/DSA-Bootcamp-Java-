0326e8cde3cbb8f58b69bca6d99ae5d21caa64c8
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-07 08:33:57 +0300

    base condition

--- src/Backtracking/Maze.java
+++ src/Backtracking/Maze.java
@@ -4,4 +4,10 @@ public class Maze {
     public static void main(String[] args) {
 
     }
+    static int count(int r, int c){
+        if(r == 1|| c == 1){
+            return 1;
+        }
+
+    }
 }
