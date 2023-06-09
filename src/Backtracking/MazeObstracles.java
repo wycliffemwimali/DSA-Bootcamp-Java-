1c58fdd694b9d205b0b114f265cc87bb023b13bd
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-09 10:55:49 +0300

    base condition

--- src/Backtracking/MazeObstracles.java
+++ src/Backtracking/MazeObstracles.java
@@ -4,4 +4,12 @@ public class MazeObstracles {
     public static void main(String[] args) {
 
     }
+
+    static void pathRestrictions(String p, boolean[][] maze, int r, int c){
+        //base condition
+        if(r == maze.length - 1 && c == maze[0].length -1){
+            System.out.println(p);
+            return;
+        }
+    }
 }
