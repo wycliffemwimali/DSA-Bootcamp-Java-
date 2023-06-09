632d4c7cf20a1f93f1c0b01487ca9224c8678df2
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-09 10:23:12 +0300

    method to return a list of possible paths

--- src/Backtracking/MazeHorizontal.java
+++ src/Backtracking/MazeHorizontal.java
@@ -1,7 +1,12 @@
 package Backtracking;
 
+import java.util.ArrayList;
+
 public class MazeHorizontal {
     public static void main(String[] args) {
+
+    }
+    static ArrayList<String> pathReturn (String p, int r, int c){
 
     }
 }
