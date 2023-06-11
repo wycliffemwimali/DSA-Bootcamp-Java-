bebb92a7e2a63a991f6d9848ff8c3b7c6c070db7
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-11 15:33:20 +0300

    bug fixed!

--- src/Backtracking/MazeObstracles.java
+++ src/Backtracking/MazeObstracles.java
@@ -19,16 +19,16 @@ public class MazeObstracles {
             System.out.println(p);
             return;
         }
+        //obstacle condiion
+        if(!maze[r][c]){
+            return;
+        }
         //recursion calls
         if(r < maze.length -1){
             pathRestrictions(p + 'D', maze, r + 1, c);
         }
         if(c < maze[0].length - 1){
             pathRestrictions(p + 'R', maze, r, c + 1);
-        }
-        //obstacle condiion
-        if(!maze[r][c]){
-            return;
         }
     }
 
