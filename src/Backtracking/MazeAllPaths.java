b8fdb256bdace5f4989e38ca04e71773f07b426b
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-11 15:47:50 +0300

    base condition

--- src/Backtracking/MazeAllPaths.java
+++ src/Backtracking/MazeAllPaths.java
@@ -5,6 +5,9 @@ public class MazeAllPaths {
 
     }
     static void allPaths(String p, boolean[][] maze, int r, int c){
+        //base condition
+        if(r == maze.length - 1 && c == maze[0].length - 1){
 
+        }
     }
 }
