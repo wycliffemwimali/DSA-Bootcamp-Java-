210ce35368e78a2e07c2e0eb69c324b5aa473f79
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-13 14:04:36 +0300

    backtracking

--- src/Backtracking/MazeAllPaths.java
+++ src/Backtracking/MazeAllPaths.java
@@ -9,6 +9,8 @@ public class MazeAllPaths {
         };
         allPaths("", board, 0, 0);
     }
+
+    //backtracking
     static void allPaths(String p, boolean[][] maze, int r, int c){
         //base condition
         if(r == maze.length - 1 && c == maze[0].length - 1){
