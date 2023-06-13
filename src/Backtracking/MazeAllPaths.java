0cf82de9673e24e60d84b06cf8b1ffcf14165e6e
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-13 14:14:17 +0300

    all path print

--- src/Backtracking/MazeAllPaths.java
+++ src/Backtracking/MazeAllPaths.java
@@ -41,4 +41,36 @@ public class MazeAllPaths {
 
         maze[r][c] = true;
     }
+
+
+    static void allPaths(String p, boolean[][] maze, int r, int c){
+        //base condition
+        if(r == maze.length - 1 && c == maze[0].length - 1){
+            System.out.println(p);
+            return;
+        }
+        //obstacle condition
+        if(!maze[r][c]){
+            return;
+        }
+
+        maze[r][c] = false;
+
+
+        //recursion calls
+        if(r < maze.length - 1){
+            allPaths(p + 'D', maze, r + 1, c);
+        }
+        if(c < maze[0].length - 1){
+            allPaths(p + 'R', maze, r, c + 1);
+        }
+        if(r > 0){
+            allPaths(p + 'U', maze, r - 1, c);
+        }
+        if(c > 0){
+            allPaths(p + 'L', maze, r, c - 1);
+        }
+
+        maze[r][c] = true;
+    }
 }
