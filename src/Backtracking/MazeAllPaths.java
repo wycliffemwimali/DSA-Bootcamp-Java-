83210e3c4c9bd5fec51a4b32f19e63d733bbb9e9
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-13 14:14:39 +0300

    all path print

--- src/Backtracking/MazeAllPaths.java
+++ src/Backtracking/MazeAllPaths.java
@@ -43,7 +43,7 @@ public class MazeAllPaths {
     }
 
 
-    static void allPaths(String p, boolean[][] maze, int r, int c){
+    static void allPathsPrint(String p, boolean[][] maze, int r, int c){
         //base condition
         if(r == maze.length - 1 && c == maze[0].length - 1){
             System.out.println(p);
