c51569eb6e1417cc041ab2e01e1212e38594c802
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-29 18:23:06 +0300

    nqueens

--- src/Backtracking/NQueens.java
+++ src/Backtracking/NQueens.java
@@ -4,4 +4,14 @@ public class NQueens {
     public static void main(String[] args) {
 
     }
+    static int queens(boolean[][] board, int row){
+        if(row == board.length){
+            display(board);
+            return 1;
+        }
+    }
+
+    private static void display(boolean[][] board) {
+
+    }
 }
