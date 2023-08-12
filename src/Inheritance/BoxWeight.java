e61da2b33f87e52a0a2e191b17ac41eaf0f05821
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-08-12 19:56:04 +0300

    super keyword

--- src/Inheritance/BoxWeight.java
+++ src/Inheritance/BoxWeight.java
@@ -6,4 +6,9 @@ public class BoxWeight extends Box{
     public BoxWeight() {
         this.weight = -3;
     }
+
+    public BoxWeight(double l, double h, double w, double weight) {
+        super(l, h, w);//initialize values in the parent class
+        this.weight = weight;
+    }
 }
