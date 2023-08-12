f6ba5470b04f369c468906617ed07d3236db80ef
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-08-12 19:40:19 +0300

    main class

---
+++ src/Inheritance/Main.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(4, 3, 4);
+
+        System.out.println(box.h + " " + box.l + " " + box.w);
+    }
+}
