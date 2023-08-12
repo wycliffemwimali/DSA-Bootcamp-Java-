b8836bad5656986a03dab5bb5a0723661d582576
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-08-12 19:50:56 +0300

    inheritance

---
+++ src/Inheritance/BoxWeight.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class BoxWeight extends Box{
+    double weight;
+
+    public BoxWeight() {
+        this.weight = -3;
+    }
+}
--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -2,8 +2,11 @@ package Inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Box box = new Box(4, 3, 4);
+//        Box box = new Box(4, 3, 4);
+//
+//        System.out.println(box.h + " " + box.l + " " + box.w);
 
-        System.out.println(box.h + " " + box.l + " " + box.w);
+        BoxWeight box1 = new BoxWeight();
+        System.out.println(box1.w + " " + box1.weight);
     }
 }
