4e1a7d8f3ba9db1ad78868119bfffe6563c2f898
wycliffemwimali <wycliffemwimali2@gmail.com>
2024-03-02 16:54:07 +0300

    multithreading

---
+++ src/Multithreading/Main.java
@@ -0,0 +1,11 @@
+package Multithreading;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.start();
+        obj2.start();
+    }
+}
