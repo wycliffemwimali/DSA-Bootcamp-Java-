63ba7cb3413ea8e80c63a26c560e6ed671744fef
wycliffemwimali <wycliffemwimali2@gmail.com>
2024-03-02 17:08:58 +0300

    thread priority and sleep

--- src/Multithreading/A.java
+++ src/Multithreading/A.java
@@ -4,6 +4,11 @@ public class A extends Thread{
     public void run(){
         for(int i=1; i<=1000;i++){
             System.out.println("hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
